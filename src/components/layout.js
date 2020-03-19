909cc47cfa4b48a181632b642cda4f2271a54b11
Richard Poulson <contact@richardpoulson.com>
2020-03-19 17:22:08 -0600

    Changed site metadata, package.json, and license

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,6 @@
 /**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
+ * Gatsby's useStaticQuery component:
+ *   https://www.gatsbyjs.org/docs/use-static-query/
  */
 
 import React from "react"
@@ -35,9 +33,7 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          © {new Date().getFullYear()}
         </footer>
       </div>
     </>
